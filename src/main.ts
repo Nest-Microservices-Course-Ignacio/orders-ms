b658decc0980ddb8b535115fbf9a62e4813d36d0
nachomartinez3010 <nachomartinez3010@gmail.com>
2025-04-01 14:48:23 -0600

    feat: add pagination and active records DTOs, configure Prisma schema, and update microservice setup

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envVars } from './config/envs';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envVars.PORT);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envVars.PORT,
+      },
+    },
+  );
 
-  console.log(`Orders MS running at ${envVars.PORT}`);
-  
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen();
+
+  Logger.log(
+    `Microservice is listening on port ${envVars.PORT}`,
+    'Orders Main',
+  );
 }
 bootstrap();
